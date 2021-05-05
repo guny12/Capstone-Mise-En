91c97cdd4e92772f36d7e95f97ac76b555e41fc3
Jim <Jimjnguy@gmail.com>
2021-05-05 18:20:08 -0400

    base line for event reducer created

--- react-app/src/store/events.js
+++ react-app/src/store/events.js
@@ -1,5 +1,49 @@
+const SET_ALLEVENTS = "events/SET_ALLEVENTS";
+const SET_SINGLEEVENT = "events/SET_SINGLEEVENT";
+
 // action creators
+const setAllEvents = (events) => ({
+	type: SET_ALLEVENTS,
+	payload: events,
+});
+
+const setSingleEvent = (event) => ({
+	type: SET_SINGLEEVENT,
+	payload: event,
+});
 
 // thunk action creators
+export const createEvent = (eventData) => async (dispatch) => {
+	const response = await csrfFetch("/api/createEvent", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(eventData),
+	});
+	if (response.ok) {
+		const data = await response.json();
+		dispatch(getEvents());
+		return data;
+	}
+};
 
 //reducer
+const initialState = {
+	currentEvent: {},
+	joinedEvents: [],
+};
+
+const eventReducer = (eventState = initialState, action) => {
+	switch (action.type) {
+		case SET_SINGLEEVENT:
+			let { JoinedEvents, CurrentEvent } = action.payload;
+			return {
+				...eventState,
+				currentEvent: CurrentEvent,
+				joinedEvents: JoinedEvents,
+			};
+		default:
+			return eventState;
+	}
+};
+
+export default eventReducer;
