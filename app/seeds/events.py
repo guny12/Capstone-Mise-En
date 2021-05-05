777350faaaa6fdd668b26ebe157c4e7ec1fe9dce
Jim <Jimjnguy@gmail.com>
2021-05-04 21:12:59 -0400

    added more variety to event Seeder

--- app/seeds/events.py
+++ app/seeds/events.py
@@ -4,6 +4,8 @@ from datetime import datetime
 faker = Faker()
 
 types=["public", "private", "catered"]
+eventName=["party", "fiesta", "get-together", "cook-off"]
+place=["house", "apartment", "loft", "yard"]
 
 def seed_events():
     for i in range(20):
@@ -12,21 +14,20 @@ def seed_events():
         hostContact= faker.phone_number(),
         hostEmail= faker.email(),
         hostURL=faker.sha256(),
-        name= f"{faker.name()} place",
-        locationName= faker.name(),
+        name= f"{faker.name()}'s {faker.words(1, eventName, True)[0]}",
+        locationName= f"{faker.name()}'s {faker.words(1, place, True)[0]}",
         location= faker.address(),
         description= faker.paragraph(nb_sentences=3),
         date= datetime.strptime(f"{faker.future_date()}","%Y-%m-%d"),
         startTime= faker.time(),
         type= faker.words(1, types, False),
-        totalCost= faker.random_int(10,1000),
+        totalCost= float(f"{faker.random_int(10,1000)}.{faker.random_int(1,9)}{faker.random_int(1,9)}"),
         availableSpots=faker.random_int(1,20),
         thingsNeeded= "None",
         hostId=1
         )
 
         db.session.add(event)
-    print(faker.date(), "FAKER DATE \n\n\n\n\n\n")
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE the users table.
