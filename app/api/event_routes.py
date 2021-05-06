669ab4322a67951ac4ff91d8f071d015edb40651
Jim <Jimjnguy@gmail.com>
2021-05-05 21:41:21 -0400

    going to remove optionals from initial create form

--- app/api/event_routes.py
+++ app/api/event_routes.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from flask import Blueprint, request,
 from app.models import User, Event, Attendee,
+from app.forms.createEvent_form import CreateEventForm
+from . import validation_errors_to_error_messages
 
 event_routes = Blueprint("event", __name__)
 
@@ -8,6 +10,13 @@ event_routes = Blueprint("event", __name__)
 # Create Event
 @event_routes.route("/", methods=["POST"])
 def create_event():
-    body = request.json
+    form = CreateEventForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+        print("THIS HAPPENED\n\n\n\n\n\n\n")
+        body = request.json
+
+
 
 # DELETE Event
