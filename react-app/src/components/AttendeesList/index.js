2cadde6d6304bbe2b292e658df0c730667fc7ddb
Jim <Jimjnguy@gmail.com>
2021-05-10 22:59:17 -0400

    added delete attendee dispatch, about to use delete attendee api route

--- react-app/src/components/AttendeesList/index.js
+++ react-app/src/components/AttendeesList/index.js
@@ -1,31 +1,37 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 // import { useHistory } from "react-router-dom";
 import "./AttendeesList.css";
 import { Alert, Badge, Button, Accordion, Card } from "react-bootstrap";
 // import * as eventActions from "../../store/event";
-// import * as attendeeActions from "../../store/attendee";
+import * as attendeeActions from "../../store/attendee";
 import EditAttendeeFormModal from "../EditAttendeeFormModal";
 
 const AttendeesList = () => {
+	const dispatch = useDispatch();
 	const listAttendees = useSelector((state) => state.attendee?.listAttendees);
 	const totalAttendees = useSelector((state) => state.attendee?.totalAttendees);
 	const numGoing = useSelector((state) => state.attendee?.numGoing);
 	const isHost = useSelector((state) => state.attendee.currentAttendee?.host);
 	const [showAlert, setShowAlert] = useState(false);
+	const [errors, setErrors] = useState([]);
 
-	const deleteAttendeeConfirm = async (attendeeId) => {};
+	const deleteAttendee = async (attendee) => {
+		setErrors([]);
+		const deleted = await dispatch(deleteAttendee(attendee));
+		if (deleted.errors) setErrors(deleted.errors);
+	};
 
 	let attendees, attendeesList;
 	if (listAttendees) {
 		attendees = Object.values(listAttendees);
 		attendeesList = attendees.map((attendee, i) => {
 			return (
-				<Card>
+				<Card key={`attendee-card-${i}`}>
 					<Accordion.Toggle as={Card.Header} eventKey={attendee.id} className="attendees-accordion-header">
-						<span className="accordion-item-name">{attendee.name}</span>{" "}
-						{attendee.host === true && <Badge variant="info">Host</Badge>}{" "}
-						{attendee.going === true && <Badge variant="success">Going</Badge>}{" "}
+						<span className="accordion-item-name">{attendee.name}</span>
+						{attendee.host === true && <Badge variant="info">Host</Badge>}
+						{attendee.going === true && <Badge variant="success">Going</Badge>}
 						{attendee.going === false && <Badge variant="danger">Not Going</Badge>}
 					</Accordion.Toggle>
 					<Accordion.Collapse eventKey={attendee.id}>
@@ -33,17 +39,26 @@ const AttendeesList = () => {
 							<p className="accordion-item-contact">
 								contact info: {attendee.contactInfo?.length >= 1 ? attendee.contactInfo : "None provided"}
 							</p>
-							{isHost && <Button variant="info">Make Host</Button>}
+							{isHost && attendee.host === false && <Button variant="info">Make Host</Button>}
 							{isHost && (
 								<Button variant="danger" onClick={() => setShowAlert(true)}>
 									Remove
 								</Button>
 							)}
+							{
+								<ul>
+									{errors.map((error, idx) => (
+										<li key={idx}>{error}</li>
+									))}
+								</ul>
+							}
 							{showAlert && (
 								<Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
 									<Alert.Heading>Remove {attendee.name}</Alert.Heading>
 									<p>Are you sure you want to remove this person?</p>
-									<Button variant="danger">Confirm Remove</Button>
+									<Button variant="danger" onClick={() => deleteAttendee(attendee)}>
+										Confirm Remove
+									</Button>
 								</Alert>
 							)}
 						</Card.Body>
