4a797c5852dcc1e1e81d3173b529647fad82082a
Jim <Jimjnguy@gmail.com>
2021-05-13 16:27:05 -0400

    completed mealplan get route

--- app/api/mealplan_routes.py
+++ app/api/mealplan_routes.py
@@ -1,9 +1,23 @@
 from datetime import datetime
 from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
-from app.models import Mealplan
+from app.models import Mealplan, Event, Items
 from app.forms.mealplan_form import CreateMealplanForm
 from . import validation_errors_to_error_messages
 
 faker = Faker()
 mealplan_routes = Blueprint("mealplan", __name__)
+
+
+# get all Mealplans that are inside an event Route
+# /mealplan/${eventid}
+@mealplan_routes.route("/<int:eventId>", methods=["GET"])
+def get_event_mealplans(eventId):
+    Mealplans = Mealplan.query.filter(Mealplan.eventId == eventId).all()
+    if Mealplans is None:
+        return {"errors": "Event does not exist"}, 400
+    mealplans = {}
+    for mealplan in Mealplans:
+        mealplans[mealplan.id] = mealplan.to_dict()
+
+    return {"Mealplans": mealplans}
