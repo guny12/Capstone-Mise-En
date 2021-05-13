25221d2a2206d9386e613e2de79c5f7d7b113cfa
Jim <Jimjnguy@gmail.com>
2021-05-13 15:42:41 -0400

    starting mealplan api/routes

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.event_routes import event_routes
 from .api.attendee_routes import attendee_routes
+from .api.mealplan_routes import mealplan_routes
 
 
 from .seeds import seed_commands
@@ -36,6 +37,7 @@ app.register_blueprint(user_routes, url_prefix="/api/users")
 app.register_blueprint(auth_routes, url_prefix="/api/auth")
 app.register_blueprint(event_routes, url_prefix="/api/event")
 app.register_blueprint(attendee_routes, url_prefix="/api/attendee")
+app.register_blueprint(mealplan_routes, url_prefix="/api/mealplan")
 
 db.init_app(app)
 Migrate(app, db)
