61b2b3aa5248281859c8951b3972b8d05597f6b8
Jim <Jimjnguy@gmail.com>
2021-05-26 05:14:47 -0400

    more styling of landing page

--- react-app/src/components/Landing/index.js
+++ react-app/src/components/Landing/index.js
@@ -26,13 +26,13 @@ const Landing = () => {
 	}, [history, sessionUser]);
 
 	const sessionLinks = (
-		<>
+		<div style={{ margin: "20px", marginLeft: "-2vw", display: "flex", justifyContent: "space-evenly", width: "40vw" }}>
 			<LoginFormModal />
 			<SignUpModal />
 			<Button variant="dark" onClick={() => handleSubmit()}>
 				Demo User
 			</Button>
-		</>
+		</div>
 	);
 
 	const handleSubmit = async () => {
@@ -44,14 +44,18 @@ const Landing = () => {
 		<div className="landing__container">
 			{!userName && (
 				<>
-					<img style={{ borderRadius: "50%" }} src={logo} alt="logo"></img>
+					<img style={{ borderRadius: "50%", margin: "25px", marginTop: "0vh" }} src={logo} alt="logo"></img>
 					<h1>{` Welcome to Mise-En`}</h1>
-					<h2>If you login, you can view all the events you are a part of</h2>
+					<h2>If you login, you can view all the events you've joined.</h2>
+					<div>{!sessionUser && sessionLinks}</div>
 				</>
 			)}
-			<h2>Create your Event and start inviting people</h2>
-			<EventFormModal />
-			<div>{!sessionUser && sessionLinks}</div>
+			<p />
+			<h2>Or just create your Event and start inviting people!</h2>
+			<div style={{ margin: "40px", marginLeft: "10vw" }}>
+				{" "}
+				<EventFormModal />
+			</div>
 		</div>
 	);
 };
