b45d510adc04a69c73cf738a97c3a68d7166eebd
Jim <Jimjnguy@gmail.com>
2021-07-15 02:30:39 -0400

    changed color of clear all button and added some margins

--- react-app/src/components/EventFormModal/EventForm.js
+++ react-app/src/components/EventFormModal/EventForm.js
@@ -7,143 +7,156 @@ import * as eventActions from "../../store/event";
 import * as attendeeActions from "../../store/attendee";
 
 const EventForm = () => {
-	const dispatch = useDispatch();
-	const history = useHistory();
-	const creatorUserId = useSelector((state) => state.session.user?.id);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const creatorUserId = useSelector((state) => state.session.user?.id);
 
-	const [errors, setErrors] = useState([]);
-	const [eventName, setEventName] = useState("");
-	const [locationName, setLocationName] = useState("");
-	const [location, setLocation] = useState("");
-	const [description, setDescription] = useState("");
-	const [date, setDate] = useState("");
-	const [startTime, setStartTime] = useState("");
-	const [type, setType] = useState("");
-	// const [totalCost, setTotalCost] = useState("");
-	// const [availableSpots, setAvailableSpots] = useState("");
-	// const [thingsNeeded, setThingsNeeded] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [eventName, setEventName] = useState("");
+  const [locationName, setLocationName] = useState("");
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [type, setType] = useState("");
+  // const [totalCost, setTotalCost] = useState("");
+  // const [availableSpots, setAvailableSpots] = useState("");
+  // const [thingsNeeded, setThingsNeeded] = useState("");
 
-	const [name, setName] = useState("");
-	const [contactInfo, setContactInfo] = useState("");
-	const [attendeeEmail, setAttendeeEmail] = useState("");
+  const [name, setName] = useState("");
+  const [contactInfo, setContactInfo] = useState("");
+  const [attendeeEmail, setAttendeeEmail] = useState("");
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setErrors([]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors([]);
 
-		const eventData = {
-			eventName,
-			locationName,
-			location,
-			description,
-			date,
-			startTime,
-			type,
-			creatorUserId,
-		};
-		// const userId = creatorUserId;
-		// const CheckEventData = await dispatch(eventActions.checkEventData(eventData));
-		// if (CheckEventData.errors) return setErrors(CheckEventData.errors);
-		// const attendeeData = { name, contactInfo, attendeeEmail, userId };
-		// const CheckAttendeeData = await dispatch(attendeeActions.checkAttendeeData(attendeeData));
-		// if (CheckAttendeeData.errors) return setErrors(CheckAttendeeData.errors);
+    const eventData = {
+      eventName,
+      locationName,
+      location,
+      description,
+      date,
+      startTime,
+      type,
+      creatorUserId,
+    };
+    // const userId = creatorUserId;
+    // const CheckEventData = await dispatch(eventActions.checkEventData(eventData));
+    // if (CheckEventData.errors) return setErrors(CheckEventData.errors);
+    // const attendeeData = { name, contactInfo, attendeeEmail, userId };
+    // const CheckAttendeeData = await dispatch(attendeeActions.checkAttendeeData(attendeeData));
+    // if (CheckAttendeeData.errors) return setErrors(CheckAttendeeData.errors);
 
-		const CurrentEvent = await dispatch(eventActions.createEvent(eventData));
-		if (CurrentEvent.errors) setErrors(CurrentEvent.errors);
-		else {
-			const userId = creatorUserId;
-			const currentEvent = CurrentEvent.CurrentEvent;
-			const attendeeAndCurrentEventData = { name, attendeeEmail, userId, contactInfo, currentEvent };
-			const CurrentAttendee = await dispatch(attendeeActions.createAttendee(attendeeAndCurrentEventData));
-			if (CurrentAttendee.errors) setErrors(CurrentAttendee.errors);
-			else {
-				history.push(`/event/${CurrentAttendee.newAttendee.attendeeURL}`);
-			}
-		}
-	};
+    const CurrentEvent = await dispatch(eventActions.createEvent(eventData));
+    if (CurrentEvent.errors) setErrors(CurrentEvent.errors);
+    else {
+      const userId = creatorUserId;
+      const currentEvent = CurrentEvent.CurrentEvent;
+      const attendeeAndCurrentEventData = {
+        name,
+        attendeeEmail,
+        userId,
+        contactInfo,
+        currentEvent,
+      };
+      const CurrentAttendee = await dispatch(
+        attendeeActions.createAttendee(attendeeAndCurrentEventData)
+      );
+      if (CurrentAttendee.errors) setErrors(CurrentAttendee.errors);
+      else {
+        history.push(`/event/${CurrentAttendee.newAttendee.attendeeURL}`);
+      }
+    }
+  };
 
-	const handleCancel = (e) => {
-		e.preventDefault();
-		setEventName("");
-		setLocation("");
-		setLocationName("");
-		setDescription("");
-		setDate("");
-		setStartTime("");
-		setType("");
-		setName("");
-		setContactInfo("");
-		setAttendeeEmail("");
-		setErrors([]);
-	};
+  const handleCancel = (e) => {
+    e.preventDefault();
+    setEventName("");
+    setLocation("");
+    setLocationName("");
+    setDescription("");
+    setDate("");
+    setStartTime("");
+    setType("");
+    setName("");
+    setContactInfo("");
+    setAttendeeEmail("");
+    setErrors([]);
+  };
 
-	return (
-		<Form onSubmit={handleSubmit}>
-			<ul>
-				{errors.map((error, idx) => (
-					<li key={idx}>{error}</li>
-				))}
-			</ul>
-			<Form.Group controlId="formBasicEventName">
-				<Form.Label>Event Name </Form.Label>
-				<Form.Control
-					type="text"
-					value={eventName}
-					onChange={(e) => setEventName(e.target.value)}
-					required
-					maxLength="100"
-					placeholder="Enter Event Name"
-				/>
-			</Form.Group>
-			<Form.Group controlId="formBasicDescription">
-				<Form.Label>Event Description </Form.Label>
-				<Form.Control
-					as="textarea"
-					rows={3}
-					value={description}
-					onChange={(e) => setDescription(e.target.value)}
-					required
-					placeholder="Enter Description"
-				/>
-			</Form.Group>
-			<Form.Row>
-				<Form.Group as={Col} controlId="formBasicDate">
-					<Form.Label>Event Date </Form.Label>
-					<Form.Control type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
-				</Form.Group>
-				<Form.Group as={Col} controlId="formBasicStartTime">
-					<Form.Label>Event Start Time </Form.Label>
-					<Form.Control
-						type="time"
-						value={startTime}
-						onChange={(e) => setStartTime(e.target.value)}
-						required
-						placeholder="Enter Start Time"
-					/>
-				</Form.Group>
-				<Form.Group as={Col} controlId="formBasicType">
-					<Form.Label>Event Type </Form.Label>
-					<Form.Control
-						type="text"
-						value={type}
-						onChange={(e) => setType(e.target.value)}
-						required
-						placeholder="ex:Private/Virtual..."
-					/>
-				</Form.Group>{" "}
-			</Form.Row>
-			<Form.Group controlId="formBasicLocationName">
-				<Form.Label>Location Name </Form.Label>
-				<Form.Control
-					type="text"
-					value={locationName}
-					onChange={(e) => setLocationName(e.target.value)}
-					required
-					maxLength="200"
-					placeholder="Enter Location Name"
-				/>
-			</Form.Group>
-			{/* <Form.Group as={Col} controlId="formTotalCost">
+  return (
+    <Form onSubmit={handleSubmit}>
+      <ul>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
+      </ul>
+      <Form.Group controlId="formBasicEventName">
+        <Form.Label>Event Name </Form.Label>
+        <Form.Control
+          type="text"
+          value={eventName}
+          onChange={(e) => setEventName(e.target.value)}
+          required
+          maxLength="100"
+          placeholder="Enter Event Name"
+        />
+      </Form.Group>
+      <Form.Group controlId="formBasicDescription">
+        <Form.Label>Event Description </Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+          placeholder="Enter Description"
+        />
+      </Form.Group>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formBasicDate">
+          <Form.Label>Event Date </Form.Label>
+          <Form.Control
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formBasicStartTime">
+          <Form.Label>Event Start Time </Form.Label>
+          <Form.Control
+            type="time"
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+            required
+            placeholder="Enter Start Time"
+          />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formBasicType">
+          <Form.Label>Event Type </Form.Label>
+          <Form.Control
+            type="text"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+            required
+            placeholder="ex:Private/Virtual..."
+          />
+        </Form.Group>{" "}
+      </Form.Row>
+      <Form.Group controlId="formBasicLocationName">
+        <Form.Label>Location Name </Form.Label>
+        <Form.Control
+          type="text"
+          value={locationName}
+          onChange={(e) => setLocationName(e.target.value)}
+          required
+          maxLength="200"
+          placeholder="Enter Location Name"
+        />
+      </Form.Group>
+      {/* <Form.Group as={Col} controlId="formTotalCost">
 					<Form.Label>Event Total Cost </Form.Label>
 					<Form.Control
 						type="number"
@@ -163,19 +176,19 @@ const EventForm = () => {
 						min="0"
 					/>
 				</Form.Group> */}
-			<Form.Group controlId="formBasicLocation">
-				<Form.Label>Location/Address </Form.Label>
-				<Form.Control
-					as="textarea"
-					value={location}
-					rows={3}
-					onChange={(e) => setLocation(e.target.value)}
-					required
-					maxLength="400"
-					placeholder="Enter Location/Address"
-				/>
-			</Form.Group>
-			{/* <Form.Group controlId="formThingsNeeded">
+      <Form.Group controlId="formBasicLocation">
+        <Form.Label>Location/Address </Form.Label>
+        <Form.Control
+          as="textarea"
+          value={location}
+          rows={3}
+          onChange={(e) => setLocation(e.target.value)}
+          required
+          maxLength="400"
+          placeholder="Enter Location/Address"
+        />
+      </Form.Group>
+      {/* <Form.Group controlId="formThingsNeeded">
 				<Form.Label>Things Needed </Form.Label>
 				<Form.Control
 					as="textarea"
@@ -185,33 +198,33 @@ const EventForm = () => {
 					placeholder="Optional Things Needed"
 				/>
 			</Form.Group> */}
-			<Form.Row>
-				<Form.Group as={Col} controlId="formBasicName">
-					<Form.Label>Your Name </Form.Label>
-					<Form.Control
-						type="text"
-						autoComplete="name"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-						required
-						maxLength="250"
-						placeholder="Enter Your Name"
-					/>
-				</Form.Group>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formBasicName">
+          <Form.Label>Your Name </Form.Label>
+          <Form.Control
+            type="text"
+            autoComplete="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            maxLength="250"
+            placeholder="Enter Your Name"
+          />
+        </Form.Group>
 
-				<Form.Group as={Col} controlId="formBasicEmail">
-					<Form.Label>Optional Email </Form.Label>
-					<Form.Control
-						type="email"
-						autoComplete="email"
-						value={attendeeEmail}
-						onChange={(e) => setAttendeeEmail(e.target.value)}
-						maxLength="200"
-						placeholder="Enter Your Email"
-					/>
-				</Form.Group>
-			</Form.Row>
-			{/* <Form.Group controlId="formBasicContactInfo">
+        <Form.Group as={Col} controlId="formBasicEmail">
+          <Form.Label>Optional Email </Form.Label>
+          <Form.Control
+            type="email"
+            autoComplete="email"
+            value={attendeeEmail}
+            onChange={(e) => setAttendeeEmail(e.target.value)}
+            maxLength="200"
+            placeholder="Enter Your Email"
+          />
+        </Form.Group>
+      </Form.Row>
+      {/* <Form.Group controlId="formBasicContactInfo">
 				<Form.Label>Optional Contact Info </Form.Label>
 				<Form.Control
 					type="text"
@@ -222,13 +235,19 @@ const EventForm = () => {
 					placeholder="Enter Optional Phone Number or other contact info"
 				/>
 			</Form.Group> */}
-			<Form.Row>
-				{/* <AttendeeFormModal eventData={eventData} /> */}
-				<Button type="submit">Create Event</Button>
-				<Button onClick={(e) => handleCancel(e)}>Clear All</Button>
-			</Form.Row>
-		</Form>
-	);
+      <Form.Row>
+        {/* <AttendeeFormModal eventData={eventData} /> */}
+        <Button type="submit">Create Event</Button>
+        <Button
+          style={{ marginLeft: "10px" }}
+          variant="danger"
+          onClick={(e) => handleCancel(e)}
+        >
+          Clear All
+        </Button>
+      </Form.Row>
+    </Form>
+  );
 };
 
 export default EventForm;
--- react-app/src/components/EventPage/index.js
+++ react-app/src/components/EventPage/index.js
@@ -11,82 +11,86 @@ import EventDetail from "../EventDetail";
 import MealplanContainer from "../MealplanContainer";
 
 const EventPage = () => {
-	const dispatch = useDispatch();
-	const attendeesLoaded = useSelector((state) => state.attendee?.loaded);
-	const mealplansLoaded = useSelector((state) => state.mealplan?.loaded);
-	const eventLoaded = useSelector((state) => state.event?.loaded);
-	const currentEventId = useSelector((state) => state.event?.currentEvent?.id);
-	const [eventAndAttendeeLoaded, setEventAndAttendeeLoaded] = useState(false);
-	const [exists, setExists] = useState(false);
-	const attendee = useSelector((state) => state.attendee.currentAttendee);
-	const attendeeURL = window.location.pathname.split("/")[2];
+  const dispatch = useDispatch();
+  const attendeesLoaded = useSelector((state) => state.attendee?.loaded);
+  const mealplansLoaded = useSelector((state) => state.mealplan?.loaded);
+  const eventLoaded = useSelector((state) => state.event?.loaded);
+  const currentEventId = useSelector((state) => state.event?.currentEvent?.id);
+  const [eventAndAttendeeLoaded, setEventAndAttendeeLoaded] = useState(false);
+  const [exists, setExists] = useState(false);
+  const attendee = useSelector((state) => state.attendee.currentAttendee);
+  const attendeeURL = window.location.pathname.split("/")[2];
 
-	useEffect(() => {
-		(async () => {
-			try {
-				const eventId = await dispatch(attendeeActions.getAttendee(attendeeURL));
-				var event = await dispatch(eventActions.getEvent(eventId));
-				if (eventId && event) setExists(true);
-				setEventAndAttendeeLoaded(true);
-			} catch {
-				setEventAndAttendeeLoaded(true);
-			}
-		})();
-	}, [dispatch, attendeeURL]);
+  // sets attendee and event in store.
+  useEffect(() => {
+    (async () => {
+      try {
+        const eventId = await dispatch(
+          attendeeActions.getAttendee(attendeeURL)
+        );
+        var event = await dispatch(eventActions.getEvent(eventId));
+        if (eventId && event) setExists(true);
+        setEventAndAttendeeLoaded(true);
+      } catch {
+        setEventAndAttendeeLoaded(true);
+      }
+    })();
+  }, [dispatch, attendeeURL]);
 
-	// updates the list of attendees if there is a change in creation or deletion.
-	useEffect(() => {
-		(async () => {
-			if (!attendeesLoaded && attendeeURL.length === 64 && exists)
-				await dispatch(attendeeActions.getAttendees(attendeeURL));
-		})();
-	}, [dispatch, attendeesLoaded, attendeeURL, exists]);
+  // updates the list of attendees if there is a change in creation or deletion.
+  useEffect(() => {
+    (async () => {
+      if (!attendeesLoaded && attendeeURL.length === 64 && exists)
+        await dispatch(attendeeActions.getAttendees(attendeeURL));
+    })();
+  }, [dispatch, attendeesLoaded, attendeeURL, exists]);
 
-	// updates the event if there is a change
-	useEffect(() => {
-		(async () => {
-			if (!eventLoaded && currentEventId && exists) await dispatch(eventActions.getEvent(currentEventId));
-		})();
-	}, [dispatch, eventLoaded, currentEventId, exists]);
+  // updates the event if there is a change
+  useEffect(() => {
+    (async () => {
+      if (!eventLoaded && currentEventId && exists)
+        await dispatch(eventActions.getEvent(currentEventId));
+    })();
+  }, [dispatch, eventLoaded, currentEventId, exists]);
 
-	// updates the list of mealplans if there is a change in creation or deletion.
-	// won't auto refresh if someone else changes it on a different page though.
-	useEffect(() => {
-		(async () => {
-			if (!mealplansLoaded && attendeeURL.length === 64 && exists)
-				await dispatch(mealplanActions.getMealplans(attendeeURL));
-		})();
-	}, [dispatch, mealplansLoaded, attendeeURL, exists]);
+  // updates the list of mealplans if there is a change in creation or deletion.
+  // won't auto refresh if someone else changes it on a different page though.
+  useEffect(() => {
+    (async () => {
+      if (!mealplansLoaded && attendeeURL.length === 64 && exists)
+        await dispatch(mealplanActions.getMealplans(attendeeURL));
+    })();
+  }, [dispatch, mealplansLoaded, attendeeURL, exists]);
 
-	// used to close the logo transition modal if it's there.
-	useEffect(() => {
-		const modal = document.querySelector("#LogoButton");
-		if (modal) modal.click();
-	}, []);
+  // used to close the logo transition modal if it's there.
+  useEffect(() => {
+    const modal = document.querySelector("#LogoButton");
+    if (modal) modal.click();
+  }, []);
 
-	if (!eventAndAttendeeLoaded) return null;
-	if (!exists && eventAndAttendeeLoaded) return <PageNotFound />;
+  if (!eventAndAttendeeLoaded) return null;
+  if (!exists && eventAndAttendeeLoaded) return <PageNotFound />;
 
-	// if you are host:
-	// list all Attendees for this event, display which ones are going or not X
-	// lets you edit event in a modal
-	// let you edit menu  and items
-	// lets you comment
+  // if you are host:
+  // list all Attendees for this event, display which ones are going or not X
+  // lets you edit event in a modal
+  // let you edit menu  and items
+  // lets you comment
 
-	// if not host:
-	// lets you view attendees, displays who's going and not x
-	// lets you edit your attendee info
-	// let you edit going or not. X
-	// lets you comment
+  // if not host:
+  // lets you view attendees, displays who's going and not x
+  // lets you edit your attendee info
+  // let you edit going or not. X
+  // lets you comment
 
-	return (
-		<div className="Event-Page-Container">
-			<EventDetail />
-			<MealplanContainer />
-			{attendeeURL.length === 64 && attendee && <AttendeeDetail />}
-			{attendeeURL.length === 64 && <AttendeesList />}
-		</div>
-	);
+  return (
+    <div className="Event-Page-Container">
+      <EventDetail />
+      <MealplanContainer />
+      {attendeeURL.length === 64 && attendee && <AttendeeDetail />}
+      {attendeeURL.length === 64 && <AttendeesList />}
+    </div>
+  );
 };
 
 export default EventPage;
